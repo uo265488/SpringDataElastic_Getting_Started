6cf674a134ab6a825840d4331d25e9cd21b3c791
Oscar Perez Arruñada <uo265488@uniovi.es>
2022-11-04 11:49:16 +0100

    Update MovieController.java

--- src/main/java/co/empathy/academy/search/controllers/MovieController.java
+++ src/main/java/co/empathy/academy/search/controllers/MovieController.java
@@ -1,6 +1,7 @@
 package co.empathy.academy.search.controllers;
 
 import co.empathy.academy.search.documents.Movie;
+import co.empathy.academy.search.parser.MultiPartToMovieListParser;
 import co.empathy.academy.search.services.MovieService;
 import jakarta.ws.rs.core.HttpHeaders;
 import org.apache.http.HttpStatus;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.function.EntityResponse;
 import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -22,29 +25,41 @@ public class MovieController {
     @Autowired
     private MovieService service;
     @PostMapping
-    public void save(@RequestBody final Movie movie) {
+    public ResponseEntity<Movie> save(@RequestBody final Movie movie) {
+        Movie newMovie = service.saveMovie(movie);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newMovie.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(newMovie);
     }
 
     @GetMapping("/{id}")
-    public Movie findById(@PathVariable final String id) {
-        return null;
+    public ResponseEntity<Movie> findById(@PathVariable final String id) {
+        Movie movie = service.findMovieById(id);
+
+        return movie == null
+                ? ResponseEntity.notFound().build()
+                : ResponseEntity.ok(movie);
     }
 
     /**
      * Indexing a movie document in the movie index
      */
     @PostMapping("/index")
-    public String indexMovie(@RequestBody Movie movie) {
+    public ResponseEntity<String> indexMovie(@RequestBody Movie movie) {
 
-        return service.indexMovie(movie);
+        return ResponseEntity.ok(service.indexMovie(movie));
     }
 
     @PostMapping("/bulking")
     public ResponseEntity<List<Movie>> bulkIndexing(@RequestParam("file") MultipartFile multipartFile) {
 
-        
+        List<Movie> movieList = new MultiPartToMovieListParser().toMovieList(multipartFile);
 
-        return ResponseEntity.ok(new ArrayList());
+        return ResponseEntity.ok(movieList);
     }
 
 }
