b10ee2d79bd688b529153cf27b8403e6079c268b
Oscar Perez Arruñada <uo265488@uniovi.es>
2022-11-18 11:25:57 +0100

    Update ResponseModel.java

--- src/main/java/co/empathy/academy/search/documents/ResponseModel.java
+++ src/main/java/co/empathy/academy/search/documents/ResponseModel.java
@@ -4,14 +4,12 @@ import co.elastic.clients.elasticsearch.core.search.Hit;
 import lombok.Value;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Value
 public class ResponseModel {
 
-    List<Movie> hits;
+    List<Object> hits;
 
     public ResponseModel(List<Hit<Movie>> hits) {
         this.hits = new ArrayList<>();
