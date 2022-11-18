277dfa983a95b93127f3d0505cbf47eaa3408507
Oscar Perez Arruñada <uo265488@uniovi.es>
2022-11-18 11:27:09 +0100

    Update MovieSearchService.java

--- src/main/java/co/empathy/academy/search/services/searching/MovieSearchService.java
+++ src/main/java/co/empathy/academy/search/services/searching/MovieSearchService.java
@@ -1,17 +1,25 @@
 package co.empathy.academy.search.services.searching;
 
+import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
 import co.empathy.academy.search.documents.ResponseModel;
 import co.empathy.academy.search.repositories.searching.MovieSearchRepository;
+import co.empathy.academy.search.repositories.searching.QueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class MovieSearchService {
 
     @Autowired
     private MovieSearchRepository repository;
+
+    @Autowired
+    private QueryFactory factory;
 
     /**
      * Calls the searchRepository to obtain the hits of the filterQuery
@@ -20,7 +28,8 @@ public class MovieSearchService {
      * @return ResponseModel
      */
     public ResponseModel filterQuery(String fieldName, String value) {
-        return new ResponseModel(repository.filterQuery(fieldName, value));
+        return new ResponseModel(repository.executeQuery(
+                factory.getFilterQuery(fieldName, value), 100));
     }
 
     /**
@@ -32,7 +41,8 @@ public class MovieSearchService {
     public ResponseModel filterQuery(String fieldName, String[] values) {
         ResponseModel responseModel = new ResponseModel(new ArrayList<>());
         for(int i = 0; i < values.length; i++) {
-            responseModel.addHits(repository.filterQuery(fieldName, values[i]));
+            responseModel.addHits(repository.executeQuery(
+                            factory.getFilterQuery(fieldName, values[i]), 100));
         }
         return responseModel;
     }
@@ -45,6 +55,8 @@ public class MovieSearchService {
      * @return
      */
     public ResponseModel rangeQuery(String fieldName, int min, int max) {
-        return new ResponseModel(repository.rangeQuery(fieldName, min, max));
+        return new ResponseModel(repository.executeQuery(
+                factory.getRangeQuery(fieldName, min, max), 100));
     }
+
 }
