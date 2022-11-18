2a8256c7a2ff61ab058aa56fc863d3256fd9c731
Oscar Perez Arruñada <uo265488@uniovi.es>
2022-11-18 11:26:54 +0100

    Update SearchRepository.java

--- src/main/java/co/empathy/academy/search/repositories/searching/SearchRepository.java
+++ src/main/java/co/empathy/academy/search/repositories/searching/SearchRepository.java
@@ -1,5 +1,6 @@
 package co.empathy.academy.search.repositories.searching;
 
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.elasticsearch.core.search.Hit;
 
 import java.util.List;
@@ -7,18 +8,9 @@ import java.util.List;
 public interface SearchRepository<Document> {
 
     /**
-     * Performs a filterQuery for the field "fieldName" and the value "value"
-     * @param fieldName
-     * @return list of hits
+     * Query executor
+     * @param query
+     * @return List<Hit<Document>>
      */
-    List<Hit<Document>> filterQuery(String fieldName, String value);
-
-    /**
-     * Performs a rangeQuery for the field "fieldName" in the range
-     * @param fieldName
-     * @return list of hits
-     */
-    List<Hit<Document>> rangeQuery(String fieldName, int min, int max);
-
-
+    List<Hit<Document>> executeQuery(Query query, int size);
 }
