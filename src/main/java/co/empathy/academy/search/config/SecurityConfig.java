e4c9d9cf76ae927ac4126ddd24acd706461120f8
Oscar Perez Arruñada <uo265488@uniovi.es>
2022-12-05 10:21:46 +0100

    Adapting

--- src/main/java/co/empathy/academy/search/config/SecurityConfig.java
+++ src/main/java/co/empathy/academy/search/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class SecurityConfig implements WebMvcConfigurer {
-    
+
      @Override
      public void addCorsMappings(CorsRegistry registry) {
          registry.addMapping("/**");
